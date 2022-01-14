10385ed946c500c222bd088f24bc2766d124f506
Jesse Hill <jesse.o.hill@gmail.com>
2022-01-13 20:32:10 -0500

    completed step two

---
+++ src/hooks/useFlip.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+
+const useFlip = () => {
+    const [flipped, setFlipped] = useState(false);
+
+    const toggle = () => {
+        setFlipped(flip => !flip);
+    }
+
+    return [flipped, toggle];
+};
+
+
+export default useFlip;
